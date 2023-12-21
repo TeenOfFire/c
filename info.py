edd54edb518e1eb26112c547d01f439fa017483a
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-12-21 18:55:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '27096551'))
 API_HASH = environ.get('API_HASH', '8600379efa072b446f9bc81da4f40786')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6007401862:AAEBr2puNIJehesIbeGyp-0KxLqPiKtkFes')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5712412804:AAEGWWHCvIK6-YVouDB2HuVRUIMNvKtfYj8')
 
 DWLD = environ.get("DWLD", 'HeroFlixx/8')
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -41,10 +41,9 @@ REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
-# MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jennie:jennie@jennie.txzzrzm.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "jennie")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'jennie')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Testing:Testing@cluster0.fkelfqj.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Ariana")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Ariana')
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'easysky.in')
